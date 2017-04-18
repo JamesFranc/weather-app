be14942d0cb37733c11638c52af0b3297e92381f
James Francis <francis.jamesii@gmail.com>
2017-04-18 14:28:31 -0500

    modified the styles a bit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <div className="Wrapper">
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Your local weather</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      </div>
       </div>
     );
   }
