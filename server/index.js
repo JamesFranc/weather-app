1d0043e2131c6ecbe81dd29e253c295c89bdde31
James Francis <francis.jamesii@gmail.com>
2017-04-23 22:04:15 -0500

    leaving the server in this state  because the data is visible

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ app.set('port', process.env.PORT || 3001);
 
 //Handler function is passed a request and response objects
 app.get('/',function(req,res){
-    var coordinates = req.state.lat+','+req.query.longitude;
-    request('https://api.darksky.net/forecast/'+key+'/'+coordinates, function (error, response, body) {
+    // var coordinates = req.state.lat+','+req.query.longitude;
+    request('https://api.darksky.net/forecast/'+key+'/33.0731,-96.8217', function (error, response, body) {
         console.log('error:', error); 
         console.log('statusCode:', response && response.statusCode);  
-        res.status(200).json(payload);
+        res.send(body);
     });
 });
 
